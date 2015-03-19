56bc6e34a6afef4e7b9256def259725b90b2598b
edd44 <kaptur.michal@gmail.com>
2015-03-19 23:04:37 +0100

    mod: drives setting cleaned up, add: onscreen setting prints

--- server_test.py
+++ server_test.py
@@ -6,8 +6,22 @@ class MyTest(unittest.TestCase):
         self.assertEqual(calc_checksum(1, 100), 2)
         self.assertEqual(calc_checksum(3, 222), 9)
         self.assertEqual(calc_checksum(0, 99), 8)
-        self.assertEqual(calc_checksum(9, 199), 8)
+        self.assertEqual(calc_checksum(9, 199), (9+1+9+9)%10)
 
     def test_packet_formatter(self):
         self.assertEqual(format_packet(1,100), "/11002\\")
         self.assertEqual(format_packet(0,222), "/02226\\")
+
+    def test_position_calc(self):
+        Config.middle_position = 150
+        Config.drive_multiplier = 25
+        self.assertEqual(calc_left_drive(0.,0.), 150)
+        self.assertEqual(calc_left_drive(-1.,0.), 175)
+        self.assertEqual(calc_left_drive(0.,-1.), 125)
+        self.assertEqual(calc_left_drive(-1.,-1.), 150)
+        self.assertEqual(calc_left_drive(1.,1.), 150)
+        self.assertEqual(calc_right_drive(0.,0.), 150)
+        self.assertEqual(calc_right_drive(-1.,0.), 125)
+        self.assertEqual(calc_right_drive(0.,-1.), 125)
+        self.assertEqual(calc_right_drive(-1.,-1.), 100)
+        self.assertEqual(calc_right_drive(1.,1.), 200)
--- vsim_server.py
+++ vsim_server.py
@@ -4,6 +4,33 @@ import sys
 import pygame
 import time
 
+'''
+Pseduo-PWM servo interface description
+
+      150
+
+180         120
+       #
+    __ # __
+	|  #  |
+	|  #  |
+	|     |
+	|     |
+	| TOP |
+	|_____|
+
+	#0 - left servo
+	#1 - right servo
+
+'''
+
+class Config:
+	drive_middle_position = 150
+	drive_multiplier = 25
+
+	screen_height = 240
+	screen_width = 320
+
 def calc_checksum(servo_nr, value):
 	s = str(int(value)).zfill(3)
 	return (int(s[0])+int(s[1])+int(s[2])+servo_nr)%10
@@ -15,10 +42,14 @@ def format_packet(servo_nr, value):
 
 def set_servo(servo_nr, value):
 	s = serial.Serial("/dev/ttyUSB0", 19200)
-	packet=format_packet(servo_nr, value)
-	print packet
-	s.write(packet)
+	s.write(format_packet(servo_nr, value))
 	time.sleep(.02)
+
+def calc_left_drive(pitch, roll):
+	return (-pitch+roll)*Config.drive_multiplier+Config.drive_middle_position
+
+def calc_right_drive(pitch, roll):
+	return (pitch+roll)*Config.drive_multiplier+Config.drive_middle_position
 
 def main():
 	pygame.joystick.init()
@@ -26,23 +57,37 @@ def main():
 
 	j.init()
 	pygame.init()
-	size = width, height = 320, 240
+	size = width, height = Config.screen_width, Config.screen_height
 	screen = pygame.display.set_mode(size)
+	font = pygame.font.SysFont("calibri",30)
 
-	middle_position = 150
-	multiplier = -50
-	position = middle_position
+	#middle_position = 150
+	#multiplier = -50
+	#position = middle_position
+
+	pitch_label_pos = (Config.screen_width / 2, 10)
+	roll_label_pos = (10, Config.screen_height / 2)
+	lservo_label_pos = (10, Config.screen_height-30)
+	rservo_label_pos = (Config.screen_width-70, Config.screen_height-30)
 
 	while 1:
 		time.sleep(.01)
 		#ISO convention {X-roll, Y-Pitch, Z-yaw}
-		pitch =-(j.get_axis(1))
-		roll =-(j.get_axis(0))
-		print str(pitch)+" "+str(roll)
-		set_servo(1, (pitch+roll)*multiplier/2+middle_position)
-		set_servo(0, (-pitch+roll)*multiplier/2+middle_position)
+		pitch =(j.get_axis(1))
+		roll =(j.get_axis(0))
+		#print str(pitch)+" "+str(roll)
+		lservo = int(calc_left_drive(pitch, roll))
+		rservo = int(calc_right_drive(pitch, roll))
+		set_servo(0, lservo)
+		set_servo(1, rservo)
 
 		for event in pygame.event.get(): # User did something
+			screen.fill((0,0,0))
+			screen.blit(font.render(str(int(pitch*100)), True,(255,255,255)),pitch_label_pos)
+			screen.blit(font.render(str(int(roll*100)), True,(255,255,255)),roll_label_pos)
+			screen.blit(font.render(str(lservo), True,(255,255,255)),lservo_label_pos)
+			screen.blit(font.render(str(rservo), True,(255,255,255)),rservo_label_pos)
+			pygame.display.flip()
 			if event.type == pygame.QUIT: sys.exit()
 
 if __name__ == "__main__":
