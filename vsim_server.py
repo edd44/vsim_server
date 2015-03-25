e15bb7f995a1022d8d393748028895263346a0c6
edd44 <kaptur.michal@gmail.com>
2015-03-25 18:06:04 +0100

    fixed indent

--- vsim_server.py
+++ vsim_server.py
@@ -12,91 +12,91 @@ Pseduo-PWM servo interface description
 180         120
        #
     __ # __
-	|  #  |
-	|  #  |
-	|     |
-	|     |
-	| TOP |
-	|_____|
+    |  #  |
+    |  #  |
+    |     |
+    |     |
+    | TOP |
+    |_____|
 
-	#0 - left servo
-	#1 - right servo
+    #0 - left servo
+    #1 - right servo
 
 '''
 
 class Config:
-	drive_middle_position = 150
-	drive_multiplier = 25
+    drive_middle_position = 150
+    drive_multiplier = 25
 
-	screen_height = 240
-	screen_width = 320
+    screen_height = 240
+    screen_width = 320
 
 def calc_checksum(servo_nr, value):
-	s = str(int(value)).zfill(3)
-	return (int(s[0])+int(s[1])+int(s[2])+servo_nr)%10
+    s = str(int(value)).zfill(3)
+    return (int(s[0])+int(s[1])+int(s[2])+servo_nr)%10
 
 def format_packet(servo_nr, value):
-	checksum = calc_checksum(servo_nr, value)
-	packet="/"+str(servo_nr)+str(int(value)).zfill(3)+str(checksum)+"\\"
-	return packet
+    checksum = calc_checksum(servo_nr, value)
+    packet="/"+str(servo_nr)+str(int(value)).zfill(3)+str(checksum)+"\\"
+    return packet
 
 def set_servo(servo_nr, value):
-	s = serial.Serial("/dev/ttyUSB0", 19200)
-	s.write(format_packet(servo_nr, value))
-	time.sleep(.02)
+    s = serial.Serial("/dev/ttyUSB0", 19200)
+    s.write(format_packet(servo_nr, value))
+    time.sleep(.02)
 
 def calc_left_drive(pitch, roll):
-	return (-pitch+roll)*Config.drive_multiplier+Config.drive_middle_position
+    return (-pitch+roll)*Config.drive_multiplier+Config.drive_middle_position
 
 def calc_right_drive(pitch, roll):
-	return (pitch+roll)*Config.drive_multiplier+Config.drive_middle_position
+    return (pitch+roll)*Config.drive_multiplier+Config.drive_middle_position
 
 def main():
-	pygame.joystick.init()
-	j = pygame.joystick.Joystick(0)
+    pygame.joystick.init()
+    j = pygame.joystick.Joystick(0)
 
-	j.init()
-	pygame.init()
-	size = width, height = Config.screen_width, Config.screen_height
-	screen = pygame.display.set_mode(size)
-	font = pygame.font.SysFont("calibri",30)
+    j.init()
+    pygame.init()
+    size = width, height = Config.screen_width, Config.screen_height
+    screen = pygame.display.set_mode(size)
+    font = pygame.font.SysFont("calibri",30)
 
-	#middle_position = 150
-	#multiplier = -50
-	#position = middle_position
+    #middle_position = 150
+    #multiplier = -50
+    #position = middle_position
 
-	pitch_label_pos = (Config.screen_width / 2, 10)
-	roll_label_pos = (10, Config.screen_height / 2)
-	lservo_label_pos = (10, Config.screen_height-30)
-	rservo_label_pos = (Config.screen_width-70, Config.screen_height-30)
+    pitch_label_pos = (Config.screen_width / 2, 10)
+    roll_label_pos = (10, Config.screen_height / 2)
+    lservo_label_pos = (10, Config.screen_height-30)
+    rservo_label_pos = (Config.screen_width-70, Config.screen_height-30)
 
-	while 1:
-		time.sleep(.01)
-		#ISO convention {X-roll, Y-Pitch, Z-yaw}
-		pitch =(j.get_axis(1))
-		roll =(j.get_axis(0))
-		#print str(pitch)+" "+str(roll)
-		lservo = int(calc_left_drive(pitch, roll))
-		rservo = int(calc_right_drive(pitch, roll))
-		set_servo(0, lservo)
-		set_servo(1, rservo)
+    while 1:
+        time.sleep(.01)
+        #ISO convention {X-roll, Y-Pitch, Z-yaw}
+        pitch =(j.get_axis(1))
+        roll =(j.get_axis(0))
+        #print str(pitch)+" "+str(roll)
+        lservo = int(calc_left_drive(pitch, roll))
+        rservo = int(calc_right_drive(pitch, roll))
+        set_servo(0, lservo)
+        set_servo(1, rservo)
 
-		for event in pygame.event.get(): # User did something
-			screen.fill((0,0,0))
+        for event in pygame.event.get(): # User did something
+            screen.fill((0,0,0))
 
-			screen.blit(font.render(str(int(pitch*100)), True,(255,255,255)),pitch_label_pos)
-			screen.blit(font.render(str(int(roll*100)), True,(255,255,255)),roll_label_pos)
+            screen.blit(font.render(str(int(pitch*100)), True,(255,255,255)),pitch_label_pos)
+            screen.blit(font.render(str(int(roll*100)), True,(255,255,255)),roll_label_pos)
 
-			screen.blit(font.render(str(lservo), True,(255,255,255)),lservo_label_pos)
-			screen.blit(font.render(str(rservo), True,(255,255,255)),rservo_label_pos)
+            screen.blit(font.render(str(lservo), True,(255,255,255)),lservo_label_pos)
+            screen.blit(font.render(str(rservo), True,(255,255,255)),rservo_label_pos)
 
-			center = [Config.screen_width/2, Config.screen_height/2]
-			pygame.draw.line(screen, (255,0,0), center, [Config.screen_width/2, Config.screen_height/2+int(pitch*100)], 10)
-			pygame.draw.line(screen, (0,255,0), center, [Config.screen_width/2+int(roll*100), Config.screen_height/2], 10)
+            center = [Config.screen_width/2, Config.screen_height/2]
+            pygame.draw.line(screen, (255,0,0), center, [Config.screen_width/2, Config.screen_height/2+int(pitch*100)], 10)
+            pygame.draw.line(screen, (0,255,0), center, [Config.screen_width/2+int(roll*100), Config.screen_height/2], 10)
 
 
-			pygame.display.flip()
-			if event.type == pygame.QUIT: sys.exit()
+            pygame.display.flip()
+            if event.type == pygame.QUIT: sys.exit()
 
 if __name__ == "__main__":
     main()
